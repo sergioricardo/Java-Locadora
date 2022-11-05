ffff95c1b0650afb1f8d0a69f0b9e9abbe25c077
Sérgio Ricardo de Oliveira <sergiofatec@gmail.com>
2022-11-05 20:52:28 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,12 @@
+import.java.util.scanner;
+
+public class Carro(){
+  public static void main(Strings[] args){
+    
+    Scanner input = new Scanner();
+    
+    System.out.print("Digite o modelo do carro: ");
+    String carro = input.NextLine();
+    
+  }
+}
